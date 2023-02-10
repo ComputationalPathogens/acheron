f95949cc876efbd50123ede621f6b26cdb95c4b7
hchen <hanxi.chen@inspection.gc.ca>
2023-02-10 11:02:52 -0700

    new file

---
+++ acheron/experimental.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+
+# imports
+def main():
+    #print('nice it runs')
+
+if __name__ == '__main__':
+    try:
+        main()
+        print('Program finished')
+    except Exception as e:
+        print('Error : {}'.format(e))
