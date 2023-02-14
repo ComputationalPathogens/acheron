eb7c7e7f710a8758dd86646787e5b147a51f6d5b
hchen <hanxi.chen@inspection.gc.ca>
2023-02-14 09:36:16 -0700

    debugged

--- acheron/experimental.py
+++ acheron/experimental.py
@@ -1,8 +1,24 @@
 #!usr/bin/env python
+import subprocess
+# micromamba activate ach.0.8
+# cd acheron
 
 # imports
 def main():
-    #print('nice it runs')
+    cmd1 = 'esearch -db nucleotide -query brucella'.split()
+    step1 = subprocess.run(cmd1, shell = False, check= False, stdout = subprocess.PIPE,
+                           stderr = subprocess.PIPE)
+    cmd2 = 'efilter -mindate 2010 -maxdate 2011 -organism mouse'.split()
+    step2 = subprocess.run(cmd2, shell = False, check = False, input = step1.stdout ,
+                           stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    cmd3 = 'efetch -format fasta'.split()
+    step3 = subprocess.run(cmd3, shell = False, check = False, input = step2.stdout,
+                           stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    print(step3.stdout)
+
+# cmd = 'efetch -db biosample -id SAMN00187850 -format fasta'.split()
+# subprocess.run(cmd)
+
 
 if __name__ == '__main__':
     try:
@@ -10,3 +26,12 @@ if __name__ == '__main__':
         print('Program finished')
     except Exception as e:
         print('Error : {}'.format(e))
+
+
+
+# python experimental.py
+# esearch -db pubmed -query "lycopene cyclase" | elink -related | elink -target protein | efilter -organism mouse -source refseq -mindate 2001 -maxdate 2010| efetch -format fasta
+#
+# esearch -db nucleotide -query "brucella" | efilter -mindate 2010 -maxdate 2011 -organism mouse| efetch -format fasta
+
+
