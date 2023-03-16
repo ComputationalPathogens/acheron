e3e25700f2bc624cb40118d7945636174ecf3dea
Chad Laing <chad.laing@inspection.gc.ca>
2023-03-16 11:57:23 -0600

    Changed creation of aho to using a map of index rather than loop for performance increase.

--- acheron/needle_in_the_hay.py
+++ acheron/needle_in_the_hay.py
@@ -17,7 +17,7 @@ def get_df_from_parquet(kmer_parquet_file):
     :return:
     """
     df = pd.read_parquet(kmer_parquet_file)
-
+    # sys.exit("done reading parquet")
     # We want to have the index be the revcomp kmer, so we can easily look up the canonical
     df = df.set_index(0)
     return df
@@ -31,23 +31,14 @@ def find_all_kmers(aho, genome_directory, kmer_df):
     :param kmer_df: dataframe from parquet file with 2 columns: reverse complements and canonical kmers
     :return: The final dataframe with rows as kmers, columns as genomes
     """
-    # Count is only for testing
-    count = 0
 
     # Create the final dataframe, removing duplicates by creating a set and then
     # converting the set into a list
     final_df = pd.DataFrame(index=[*set(kmer_df[1])])
 
     for gz_file in pathlib.Path(genome_directory).glob('*.gz'):
-
-        # <-This is just for testing to limit the number of genomes and print the df
-
-        count += 1
-        if count == 5:
-            print(final_df)
-            return final_df
-        # ->
-
+        print("Reading {}".format(gz_file))
+        
         with gzip.open(gz_file, 'rt') as fasta:
             # New genome column, currently names as file name
             # Default every kmer to 0 for each genome
@@ -74,12 +65,10 @@ def create_aho_automaton(kmer_df):
     """
     aho = ahocorasick.Automaton()
 
-    # The index is the revcomp kmer
-    for i in kmer_df.index:
-        # We look up the canonical kmer to add to the automaton
-        k2 = kmer_df.loc[i, 1]
-        aho.add_word(i, i)
-        aho.add_word(k2, k2)
+    # The index contains both canonical and revcomp kmers
+    # kmer_df[1] contains only canonical kmers
+
+    kmer_df.index.map(lambda x: aho.add_word(x,x))
     aho.make_automaton()
     return aho
 
@@ -113,9 +102,11 @@ def main():
         sys.exit(1)
 
     kmer_df = get_df_from_parquet(kmer_parquet_file)
+    print("DF read")
     aho = create_aho_automaton(kmer_df)
+    print("aho created")
     final_df = find_all_kmers(aho, directory_of_genomes, kmer_df)
-    # print(final_df)
+    print("final df made")
     # Still need to create a function to save the output table ...
     save_parquet1(final_df, output_file)
 
